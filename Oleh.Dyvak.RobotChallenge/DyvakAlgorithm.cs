b4e8657c2eced1f2d1fd0eb29c440bf49feb5a8a
Oleh Dyvak <vodyvak@gmail.com>
2018-09-04 23:33:16 +0300

    commit

---
+++ Oleh.Dyvak.RobotChallenge/DyvakAlgorithm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+//using Oleh.Dyvak.RobotChallenge.DistanceHelper;
+
+namespace Oleh.Dyvak.RobotChallenge
+{
+    public class DyvakAlgorithm : IRobotAlgorithm
+    {
+        public string Author
+        {
+            get { return "Dyvak Oleh"; }
+        }
+
+        public string Description
+        {
+            get { return "Sample"; }
+        }
+
+        public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
+            if ((movingRobot.Energy > 500) && (robots.Count < map.Stations.Count))
+            {
+                return new CreateNewRobotCommand();
+            }
+            Position stationPosition = DistanceHelper.FindNearestFreeStation(robots[robotToMoveIndex], map, robots);
+            if (stationPosition == null)
+                return null;
+            if (stationPosition == movingRobot.Position)
+                return new CollectEnergyCommand();
+            else
+            {
+                return new MoveCommand() { NewPosition = stationPosition };
+            }
+        }
+
+    }
+
+    
+
+}
